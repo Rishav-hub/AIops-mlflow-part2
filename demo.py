cedbb087287bafb5906b818ecadda4b8e7b289a6
Rishav-hub <9930046@gmail.com>
2022-03-01 18:30:31 +0530

    sklearn model implemented

--- demo.py
+++ demo.py
@@ -3,29 +3,59 @@ import os
 import mlflow
 import argparse
 import time
-def evaluate(param1,param2):
-    metric = param1**2 + param2 ** 2
-    return metric
+import pandas as pd
+from sklearn.metrics import mean_squared_error, r2_score,mean_absolute_error
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+import numpy as np
 
-def main(p1, p2):
+def evaluate(actual,pred):
+    rmse = np.sqrt(mean_squared_error(actual, pred))
+    mae = mean_absolute_error(actual, pred)
+    r2 = r2_score(actual, pred)
+    return rmse,mae,r2
+
+def get_data():
+    URL = "http://archive.ics.uci.edu/ml/machine-learning-databases/wine-quality/winequality-red.csv"
+    try:
+        df = pd.read_csv(URL, sep=";")
+        return df
+    except Exception as e:
+        raise e
+
+def main(alpha, l1_ratio):
+    df = get_data()
+    train, test = train_test_split(df)
+
+    TARGET = "quality"
+
+    train_x = train.drop([TARGET], axis=1)
+    test_x = test.drop([TARGET], axis=1)
+
+    train_y = train[[TARGET]]
+    test_y = test[[TARGET]]
     with mlflow.start_run():
-        metric = evaluate(p1, p2)
-        mlflow.log_param("param1", p1)
-        mlflow.log_param("param2", p2)
-        mlflow.log_metric("metric", metric)
+        lr_model = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+        lr_model.fit(train_x, train_y)
+        pred = lr_model.predict(test_x)
+        rmse, mae, r2 = evaluate(test_y, pred)
+        mlflow.log_param("alpha", alpha)
+        mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("mae", mae)
+        mlflow.log_metric("r2", r2)
 
-        os.makedirs("temp", exist_ok=True)
-        with open("temp/time.txt", "w") as f:
-            f.write(time.asctime())
-        mlflow.log_artifact("temp/time.txt")
+        print(f"params- alpha: {alpha}, l1_ratio: {l1_ratio}")
+        print(f"eval metrics - rmse: {rmse}, mae: {mae}, r2: {r2}")
 
+        mlflow.sklearn.log_model(lr_model, "model")
 
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
-    args.add_argument("--param1","-p1", type=int, default=2)
-    args.add_argument("--param2","-p2", type=int, default=5)
+    args.add_argument("--alpha","-a", type=float, default=0.5)
+    args.add_argument("--l1_ratio","-l1", type=float, default=0.5)
     parsed_args = args.parse_args()
 
     # Set the experiment name and run
-    main(parsed_args.param1, parsed_args.param2)
+    main(parsed_args.alpha, parsed_args.l1_ratio)
--- runs.py
+++ runs.py
@@ -1,9 +1,12 @@
 import os
 import mlflow
-p1s = range(1,5)
-p2s = range(1,5)
+import numpy as np
 
-for p1 in p1s:
-    for p2 in p2s:
+alphas = np.linspace(0.1,1.0, 5)
+l1_ratios = np.linspace(0.1,1.0, 5)
+
+
+for p1 in alphas:
+    for p2 in l1_ratios:
         print(f"Logging experiment for p1 = {p1} and p2 = {p2}")
-        os.system(f"python demo.py -p1 {p1} -p2 {p2}")
+        os.system(f"python demo.py -a {p1} -l1 {p2}")
